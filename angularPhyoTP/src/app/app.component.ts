94ea8f45293e2304a7ac94e85f6efd060267075d
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-04-27 07:29:14 +0000

    added routes, avenir next rounded, home component and changed some colors

--- angularPhyoTP/src/app/app.component.ts
+++ angularPhyoTP/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
