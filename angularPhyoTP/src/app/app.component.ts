990ccc3c2e8d5cb8fd0a8ea8912af476a43a1031
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-05-04 05:00:31 +0000

    fix home and changed website details

--- angularPhyoTP/src/app/app.component.ts
+++ angularPhyoTP/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterModule],
+  imports: [RouterOutlet, HeaderComponent, RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularPhyoTP';
+  title = "PhyoTP's Portfolio";
 }
