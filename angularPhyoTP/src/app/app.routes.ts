e06eb685978460db46bc59052ddce80717cef03c
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-05-27 03:36:16 +0000

    fixing error page, centering stuff, new buttons and added new intro

--- angularPhyoTP/src/app/app.routes.ts
+++ angularPhyoTP/src/app/app.routes.ts
@@ -5,5 +5,6 @@ import { ErrorComponent } from './error/error.component';
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'error', component:ErrorComponent},
     { path: '**', component: ErrorComponent }
 ];
