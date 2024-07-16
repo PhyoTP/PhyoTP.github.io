37d486b51c28b83e669415fa3009d5c1e977595b
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-07-16 13:54:44 +0000

    change some stuff

--- angularPhyoTP/src/imgbutton.ts
+++ angularPhyoTP/src/imgbutton.ts
@@ -20,10 +20,10 @@ export const codingLangs: ImageButton[] = [
     createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/Swift/Swift.png', 'Swift', '/swift'),
     createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/Python/Python.png', 'Python', '/python'),
     createImageButton('https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Sql_data_base_with_logo.png/400px-Sql_data_base_with_logo.png', 'SQL', '/sql'),
-    createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/Html/HTML.png', 'HTML', '/html'),
-    createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/CSS%E5%AE%8C%E5%85%A8%E3%81%AB%E7%90%86%E8%A7%A3%E3%81%97%E3%81%9F/CSS%E5%AE%8C%E5%85%A8%E3%81%AB%E7%90%86%E8%A7%A3%E3%81%97%E3%81%9F.png', 'CSS', '/css'),
-    createImageButton('https://raw.githubusercontent.com/andregans/code_logotype/main/Javascript%20Logotype.png', 'JavaScript', '/js','JS'),
-    createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/TypeScript/TypeScript.png', 'TypeScript', '/typescript','TS'),
+    createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/Html/HTML.png', 'HTML', '/webdev/#html'),
+    createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/CSS%E5%AE%8C%E5%85%A8%E3%81%AB%E7%90%86%E8%A7%A3%E3%81%97%E3%81%9F/CSS%E5%AE%8C%E5%85%A8%E3%81%AB%E7%90%86%E8%A7%A3%E3%81%97%E3%81%9F.png', 'CSS', '/webdev/#css'),
+    createImageButton('https://raw.githubusercontent.com/andregans/code_logotype/main/Javascript%20Logotype.png', 'JavaScript', '/webdev/#js','JS'),
+    createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/TypeScript/TypeScript.png', 'TypeScript', '/webdev/#ts','TS'),
     createImageButton('https://devforum-uploads.s3.dualstack.us-east-2.amazonaws.com/uploads/optimized/5X/d/4/1/3/d413db5053563c9efdaa6d9c92c24a90110ed165_2_690x420.png', 'Luau', '/luau'),
     createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/C%23/C%23.png', 'C#', '/csharp','CS')
 ];
@@ -37,7 +37,7 @@ export const technologies: ImageButton[] = [
     createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/GitHub/GitHub.png', 'GitHub', '/git/#github'),
     createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/Figma/Figma.png', 'Figma', '/figma'),
     createImageButton('https://preview.redd.it/made-this-roblox-studio-logo-v0-iidroe6u835a1.png?width=640&crop=smart&auto=webp&s=ca902e12fbdd3718dfea23099c7b6abf22a2063f', 'Roblox Studio', '/luau/#robloxstudio','roblox-studio'),
-    createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/Angular/Angular.png', 'Angular', '/angular'),
+    createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/Angular/Angular.png', 'Angular', '/webdev/#angular'),
     createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/UnityBlender/UnityBlenderT.png', 'Unity', '/cs/#unity'),
     createImageButton('https://raw.githubusercontent.com/Ender-Wiggin2019/ServiceLogos/main/VisualStudio/VisualStudio.png', 'Visual Studio', '/visual-studio','VS'),
     createImageButton('https://raw.githubusercontent.com/Aikoyori/ProgrammingVTuberLogos/main/Docker/DockerLogo.png', 'Docker', '/docker')
@@ -45,7 +45,7 @@ export const technologies: ImageButton[] = [
 
 export const projects: ImageButton[] = [
     createImageButton('/assets/academ.png', 'Academ', '/swift/#academ'),
-    createImageButton('/assets/portfolio.png', 'My Portfolio', '/phyotp',"PhyoTP"),
+    createImageButton('/assets/portfolio.png', 'My Portfolio', '/webdev/#portfolio',"Portfolio"),
     createImageButton('/assets/ftcl.png', 'Find The Code Langs', '/luau/#ftcl','FTCL'),
     createImageButton('/assets/bouncer.png', 'Bouncer', '/cs/#bouncer')
 ];
